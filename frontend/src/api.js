e50ce7a79452305e05c73984f84bbe3a9e81d02b
Ifeanyi Nwagbara <95752682+IfeanyiXtopher@users.noreply.github.com>
2024-05-16 23:13:18 +0100

    4th  commit backend url updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,17 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+
+const apiUrl = "/choreo-apis/djangoreactnote/backend/rest-api-be2/v1" //this is for deployment on choreo. not part of the main code
+
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl, //this also included b/c of const apiUrl above. the main one is below commented
 })
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL
+// })
 
 api.interceptors.request.use(
     (config) => {
